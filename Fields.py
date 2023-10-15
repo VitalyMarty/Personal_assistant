71c125b85eed88f7c095ce1e00092ccc853a43ce
Vitalii Savenko <vitaly.savenko12@gmail.com>
2023-10-15 10:21:28 +0300

    add_function_for fields

--- Fields.py
+++ Fields.py
@@ -1,24 +1,71 @@
+from datetime import datetime
 
 
-class Fields:
+class Field:
+    def __init__(self, value):
+        self._value = value
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = self.validate(self.normalize(value))
+
+    def normalize(self, value: str) -> str:
+        return value
+    
+    def validate(self, value: str) -> str:
+        return value
+
+    def __str__(self):
+        return f'{self._value}'
+
+
+class Name(Field):
+
+    def normalize(self, name: str) -> str:
+        return name.title()
+
+
+class Phone(Field):
+
+    def normalize(self, value: str) -> str:
+        new_value = (
+            value.removeprefix("+")
+                .replace("(", "")
+                .replace(")", "")
+                .replace("-", "")
+                .replace(" ", "")
+        )
+        return new_value
+    
+    def validate(self, value: str) -> str:
+        if len(value) < 10 or len(value) > 12:
+                return f"Phone '{value}' must contains 10 symbols."
+        if not value.isnumeric():
+                return 'Wrong phones.'
+        return value
+
+
+class Birthday(Field):
+
+    def normalize(self, birthday: str) -> str:
+        normal_birthday = datetime.strptime(birthday, '%Y-%m-%d').date()                    # TODO Якщо дата не в правильному форматі викличеться ValueError. Її треба якось обробити
+        return normal_birthday
+
+    def validate(self, birthday: str) -> str:
+        today = datetime.now().date()
+        if birthday > today:
+            return f"Birthday '{birthday}' must be less than current year and date."
+        return birthday
+
+
+class Address(Field):
     pass
 
 
-class Name(Fields):
-    pass
-
-
-class Phone(Fields):
-    pass
-
-
-class Birthday(Fields):
-    pass
-
-
-class Address(Fields):
-    pass
-
-
-class Email(Fields):
+class Email(Field):
     pass
