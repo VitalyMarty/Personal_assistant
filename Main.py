0a540a605bacb732305ae9a705c727dbafa404c9
VitalyMarty <vitalymarty@gmail.com>
2023-10-15 12:00:26 +0300

    Updated main, parcer and dict

--- Main.py
+++ Main.py
@@ -1,13 +1,25 @@
-from AddressBook import AddressBook, Record
-from NoteBook import NoteBook, Note
-from Fields import Name, Phone, Email, Birthday, Address
-from Backup import backup_data
+import AddressBook
+import NoteBook
+
+get_help = {
+    'hello': "How can I help you?",
+    'add': "Add a contact to the address book (usage: add name phone birthday)",
+    'change': "Change contact information (usage: change name phone)",
+    'phone': "Get the phone number for a contact (usage: phone name)",
+    'search': "Search for contacts (usage: search query)",
+    'add_note': "Add a note to the notebook (usage: add_note title content tags)",
+    'edit_note': "Edit a note (usage: edit_note title content tags)",
+    'delete_note': "Delete a note (usage: delete_note title)",
+    'list_contacts': "List all contacts",
+    'list_notes': "List all notes",
+    'goodbye': "Exit the program",
+}
 
 def main():
     contacts = AddressBook()
     notes = NoteBook()
 
-    print("Bot assistant is running. Type 'exit' to exit.")
+    print("Bot assistant is running. Type 'help' to view commands. Type 'exit' to exit.")
     while True:
         command = input("Enter a command: ")
         try:
@@ -19,100 +31,77 @@ def main():
 
 def parse_command(contacts, notes, command):
     command = command.lower()
-    
+    command_parts = command.split()
+
     if command == 'hello':
         return "How can I help you?"
+    
     elif command.startswith('add '):
         _, data = command.split(' ', 1)
         parts = data.split()
         if len(parts) != 3:
             return "Provide name, phone, and birthday (if applicable)"
         name, phone, birthday = parts
-        contacts.add_record(Record(name, birthday))
-        contacts.data[name].add_phone(phone)
-        return f"Added contact: {name}, {phone}, Birthday: {birthday}"
+        return AddressBook.add_contact(contacts, name, phone, birthday)
+    
     elif command.startswith('change '):
         _, data = command.split(' ', 1)
-        if ' ' not in data:
+        if len(command_parts) != 3:
             return "Provide name and phone please"
         name, phone = data.split()
-        contact = contacts.find(name)
-        if contact:
-            contact.edit_phone(contact.phones[0].value, phone)
-            return f"Updated contact: {name}, {phone}"
-        else:
-            raise ValueError(f"Contact '{name}' not found.")
+        return AddressBook.change_contact(contacts, name, phone)
+    
     elif command.startswith('phone '):
-        *_, name = command.split(' ')
-        contact = contacts.find(name)
-        if contact:
-            return f"Phone number for {name}: {contact.phones[0].value}"
-        else:
-            raise ValueError(f"Contact '{name}' not found.")
+        if len(command_parts) != 2:
+            return "Provide name please"
+        name = command_parts[1]
+        return AddressBook.get_phone(contacts, name)
+    
     elif command.startswith('search '):
         _, query = command.split(' ', 1)
-        results = contacts.search(query)
-        if results:
-            return "\n".join([str(record) for record in results])
-        else:
-            return "No matching contacts found."
-    elif command.startswith('add contact '):
-        _, data = command.split(' ', 2)
-        name, phone, birthday = data.split()
-        contacts.add_record(Record(name, birthday))
-        contacts.data[name].add_phone(phone)
-        return f"Added contact: {name}, {phone}, Birthday: {birthday}"
-    elif command.startswith('add note '):
-        _, data = command.split(' ', 2)
+        return AddressBook.search_contacts(contacts, query)
+    
+    elif command.startswith('add_note '):
+        _, data = command.split(' ', 1)
         title, content, tags = data.split()
-        note = Note(title, content)
-        note.add_tags(tags.split(','))
-        notes.add_note(note)
-        return f"Added note: {title}, Tags: {', '.join(note.tags)}"
-    elif command.startswith('edit note '):
-        _, title, content, tags = command.split(' ', 3)
-        note = notes.find(title)
-        if note:
-            note.edit_content(content)
-            note.clear_tags()
-            note.add_tags(tags.split(','))
-            return f"Updated note: {title}, Tags: {', '.join(note.tags)}"
-        else:
-            return f"Note '{title}' not found."
-    elif command.startswith('delete note '):
-        _, title = command.split(' ', 1)
-        notes.delete(title)
-        return f"Deleted note: {title}"
-    elif command.startswith('list contacts'):
-        return "\n".join([str(record) for record in contacts])
-    elif command.startswith('list notes'):
-        return "\n".join([str(note) for note in notes])
-    elif command in ['good bye', 'close', 'exit']:
+        return NoteBook.add_note(notes, title, content, tags)
+    
+    elif command.startswith('edit_note '):
+        _, data = command.split(' ', 1)
+        title, content, tags = data.split()
+        return NoteBook.edit_note(notes, title, content, tags)
+    
+    elif command.startswith('delete_note '):
+        if len(command_parts) != 2:
+            return "Provide the title of the note to delete"
+        title = command_parts[1]
+        return NoteBook.delete_note(notes, title)
+    
+    elif command == 'list_contacts':
+        return AddressBook.list_contacts(contacts)
+    
+    elif command == 'list_notes':
+        return NoteBook.list_notes(notes)
+    
+    elif command in ['goodbye', 'close', 'exit']:
         print("Good bye!")
-        backup_data(contacts, notes)
+        AddressBook.backup_data(contacts)
+        NoteBook.backup_data(notes)
         exit()
-    elif command in ['add', 'change']:
-        return "Provide name and phone please"
-    elif command == 'phone':
-        return "Provide name please"
-    else:
-        return "Unknown command."
+        
+    elif command == 'help':
+        return get_help_text()
+
+    return "Unknown command."
+
+def get_help_text():
+    help_text = "Available commands:\n"
+    for command, description in get_help.items():
+        help_text += f"{command}: {description}\n"
+    return help_text
 
 if __name__ == "__main__":
     main()
 
-
-
-# command_dict = {
-#     'hello': "How can I help you?",
-#     'add contact': "Add a contact to the address book",
-#     'add note': "Add a note to the notebook",
-#     'change contact': "Change contact information",
-#     'edit note': "Edit a note",
-#     'delete note': "Delete a note",
-#     'list contacts': "List all contacts",
-#     'list notes': "List all notes",
-#     'good bye': "Exit the program",
-# }
 
 # completer = WordCompleter(command_dict.keys())
