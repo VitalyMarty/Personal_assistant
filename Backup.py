d46225086f6ff2d7738f00a7d8e8de4511e00c10
Vitalii Savenko <vitaly.savenko12@gmail.com>
2023-10-16 00:21:05 +0300

    add functional backup

--- Backup.py
+++ Backup.py
@@ -1,17 +1,5 @@
 import pickle
 
-# def save_address_book(address_book, file_name):
-#     with open(file_name, 'wb') as file:
-#         pickle.dump(address_book, file)
-# def load_address_book(file_name):
-#     try:
-#         with open(file_name, 'rb') as file:
-#             address_book = pickle.load(file)
-#         return address_book
-#     except FileNotFoundError:
-#         # Повернути новий об'єкт адресної книги, якщо файл не знайдено.
-#         return addressbook()
-
 class Storage:
     def save(self):
         pass
