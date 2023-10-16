080032c6d4f38fafcc31e00c2418d936686f4a00
Artificer-ua <metal.work3r@gmail.com>
2023-10-16 16:34:05 +0300

    Notebook. Add and remove notes

--- NoteBook.py
+++ NoteBook.py
@@ -1,72 +1,104 @@
 from collections import UserDict
+from Fields import Field
+from datetime import datetime
+import re
 
 
-class Note:
-    def __init__(self, text):
-        self._text = None
-        self.text = text
+class Note(Field):
+    def __init__(self, value: str) -> None:
+        # TODO restore super when fix in Fields
+        super().__init__(value)
+        # self._value = None
+        # self.value = value
 
-    def __str__(self):
-        return str(self.text)
 
-    @property
-    def text(self):
-        return self._text
-
-    # checking input (if needed)
-    @text.setter
-    def text(self, text):
-        self._text = text
+    # @Field.value.setter
+    # def value(self, value):
+    #     if re.match(r"^(.{10,100})$", value):
+    #         self._value = value
+    #     else:
+    #         print("Note must be in range of 10-100 symbols.")
 
 
 # Class for Tags. Only for check correct input
 # only words accepted due to technical assignment
-class Tag(Note):
-    def __init__(self, text):
-        super().__init__(text)
+class Tag(Field):
+    def __init__(self, value) -> None:
+        # TODO restore super when fix in Fields
+        super().__init__(value)
+        # self._value = None
+        # self.value = value
 
-#TODO add input check
-    @Note.text.setter
-    def text(self, text):
-        self._text = text
+    # check input
+    @Field.value.setter
+    def value(self, value) -> None:
+        if re.match(r"^[A-Za-z]{3,10}$", value):
+            self._value = value
+        else:
+            print("Incorrect Tag format. 3-10 letters, without digits, spaces and special symbols accepted.")
 
 
 class NoteRecord:
-    def __init__(self, text):
+    def __init__(self, text: str) -> None:
         self.note = Note(text)
-        self.tag = []
+        # set time when record is created
+        self.edit_date = datetime.now()
+        self.tags = []
 
-    def __str__(self):
-        return "Class NoteRecord str method"
+    def __str__(self) -> str:
+        return str(self.note)
 
     # return __repr__ as string
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
+
+    # add one tag per one call to one note
+    def add_tag(self, tag: str) -> str:
+        # checking length of tags[] to test if Tag setter allow value
+        add_check = len(self.tags)
+        self.tags.append(Tag(tag))
+        if len(self.tags) > add_check:
+            return "Tag is added successfully."
+        else:
+            return "Tag is not added."
 
 
 class Notebook(UserDict):
     # using for calling iter with parameters
     def __init__(self, dict=None):
         super().__init__(dict)
+        # notes counter
+        self.counter = 0
 
-    # not needed for this class. there are no problematic attributes
     # placeholder __getstate__
     def __getstate__(self):
         attributes = {**self.__dict__}
         return attributes
 
-    # not needed too
     # placeholder __setstate__
     def __setstate__(self, value):
         self.__dict__ = value
 
-    # TODO adding note
-    def add_note(self, *args, **kwargs) -> str:
-        return "Return from class Notebook function add_note"
+    # adding note
+    def add_note(self, record: NoteRecord) -> str:
+        # set note number
+        self.counter += 1
+        self.data[self.counter] = record
+        return f"Note #{self.counter} is successfully added"
 
-    # TODO remove note
-    def remove_note(self, *args, **kwargs) -> str:
-        return "Return from class Notebook function remove_note"
+    # remove note
+    def remove_note(self, id: int) -> str:
+        if id <= 0 or id > self.counter:
+            return f"Incorrect note number. Note number must be in range 1 to {self.counter}" if self.counter \
+                else f"You have {self.counter} notes. Nothing to delete."
+        # del note record
+        self.data.pop(id)
+        # decrease notes counter
+        self.counter -= 1
+        # recalculate notes ID (dict keys)
+        for i in range(id, self.counter+1):
+            self.data[i] = self.data.pop(i+1)
+        return f"Note with id {id} is successfully deleted."
 
     # TODO edit notes
     def edit_note(self, *args, **kwargs) -> str:
@@ -75,10 +107,6 @@ class Notebook(UserDict):
     # TODO find note
     def find_note(self, *args, **kwargs) -> str:
         return "Return from class Notebook function find_note"
-
-    # TODO add tag|tags to note
-    def add_tag(self, *args, **kwargs) -> str:
-        return "Return from class Notebook function add_tag"
 
     # TODO find note by tag
     def find_by_tag(self, *args, **kwargs) -> str:
@@ -90,4 +118,21 @@ class Notebook(UserDict):
 
 if __name__ == "__main__":
     print("Using for testing")
-    
+    record1 = NoteRecord("some text1")
+    print(record1)
+    record2 = NoteRecord("some text2")
+    print(record2)
+    record3 = NoteRecord("some text3")
+    print(record3)
+
+    print(record1.add_tag("aaa"))
+    print(record1.tags[0])
+
+    print(record1.edit_date)
+
+    book = Notebook()
+    print(book.add_note(record1))
+    print(book.add_note(record2))
+    print(book.add_note(record3))
+    print("Count of notes: ", book.counter)
+    print(book.remove_note(1))
