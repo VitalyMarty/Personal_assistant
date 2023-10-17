2e0a550cf0ce07a8cc1bd9e144d2f53992d749ff
Vitalii Savenko <vitaly.savenko12@gmail.com>
2023-10-17 23:47:42 +0300

    rename imports

--- src/tough_assistant/main.py
+++ src/tough_assistant/main.py
@@ -1,8 +1,8 @@
-from AddressBook import AddressBook
-from Backup import Backup, PickleStorage
-from Commands import command_dict
-from NoteBook import Notebook
-from Decorators import input_error
+from address_book import AddressBook
+from backup import Backup, PickleStorage
+from commands import command_dict
+from note_book import Notebook
+from decorators import input_error
 
 
 
