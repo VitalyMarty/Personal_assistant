b66d00b8f4ff69c7fc0739ee849ffa0508cb8553
VitalyMarty <vitalymarty@gmail.com>
2023-10-17 23:42:26 +0300

    test2

--- src/tough_assistant/Main.py
+++ src/tough_assistant/Main.py
@@ -5,6 +5,7 @@ from NoteBook import Notebook
 from Decorators import input_error
 
 
+
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.shortcuts import input_dialog
