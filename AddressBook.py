edad4b8f3a6d4a27518c4c5afba8272e542f7d82
VitalyMarty <137708176+VitalyMarty@users.noreply.github.com>
2023-10-17 22:34:33 +0300

    Revert "New branch feature 1"

--- AddressBook.py
+++ AddressBook.py
@@ -1,6 +1,7 @@
 from collections import UserDict
 from Fields import Address, Birthday, Email, Name, Phone  
-from Backup import Backup, PickleStorage
+from datetime import date
+import pickle
 
 
 class AddressBook(UserDict):
@@ -40,17 +41,7 @@ class AddressBook(UserDict):
             if name:
                 dict_contacts[name] = days
                 
-        return dict_contacts
-
-    def show_contacts(self):
-        message = ''
-        for count, contact in enumerate(self.data.values()):
-            if count < 9:
-                count = f'0{count}'
-            row = f'{count}.{contact.name.value}'
-            message = '\n'.join([message, row])
-        print(message)
-        return message              
+        return dict_contacts              
 
 
 class Record:
@@ -190,8 +181,6 @@ class Record:
         return f'Record({self.name.value}, {self.phones_repr}, {self.birthday.value})'
     
     
-# Створюємо сховище, де зберігається файл з контактами та нотатками
-storage_addressbook = Backup(PickleStorage('test_addressbook.pickle'))
-# Завантажуємо контакти та нотатки з файлів. Якщо файли відсутні створюємо нові.
-contacts = AddressBook() if storage_addressbook.load() is None else storage_addressbook.load()
+        
+
 
--- Commands.py
+++ Commands.py
@@ -1,11 +1,19 @@
-from AddressBook import Record, contacts
-from NoteBook import notes
 
-def add_contact(name):
-    """'add phone <name>' Добавити контакт"""
-    record = Record(name)
-    contacts.add_record(record)
-    return f'Added contact {name} to contacts'
+
+def add():
+    """
+    add phone <name> <phone>
+    add birthday <name> <date>
+    add address <name> <address>
+    add email <name> <email>
+    add note <note>
+    add tag <note> <tag>
+    
+    Функція повертає:
+    - Повідомлення, що <об'єкт> було добавлено в <місце>
+    - повідомлення про помилку
+    """
+    pass
 
 
 def change():
@@ -64,14 +72,25 @@ def get_help():
     """Show available commands"""
     message = 'I can do next commands:\n'
     for count, command in enumerate(command_dict, start=1):
-        if count <= 9:
-            count = f'0{count}'
-        message = '\n'.join([message, f'{count}. {command:<17}-{command_dict[command].__doc__}'])
+        message = '\n'.join([message, f'{count}. {command:<10}-{command_dict[command].__doc__}'])
 
     return message
 
 
-def find_birthdays_in_x_days(days):
+# def get_next_birthday():
+#     """
+#     birthday <name (можна частково)>
+    
+#     Функція повертає:
+#     - Кількість днів до наступного дня народження, якщо в записі вказано день народження.
+#     - Повідомлення, що для такого запису еднь народження не задано
+#     - Повідомлення, що такого запису <name> не існує.
+    
+#     Якщо реалізовувати частковий пошук, то мабуть треба щоб виводило перелік записів, що відповідають критерію.
+#     """
+#     pass
+
+def find_birthdays_in_x_days(contacts, days):
     matching_contacts = ''
     dict_contacts = contacts.check_birthday(days)
     for name in dict_contacts:
@@ -106,9 +125,15 @@ def sort_dir():
     pass
 
 
-def show_contacts():
-    """Вивести всі збереженні контакти"""
-    return contacts.show_contacts()
+def show_all():
+    """
+    Користувач вводить наступні команди через пробіл:
+    show all
+    
+    Функція повертає:   
+    - всі збереженні контакти з номерами телефонів у консоль.
+    """
+    pass
 
 
 command_dict ={
@@ -116,27 +141,5 @@ command_dict ={
     'exit': goodbye,
     'close': goodbye,
     'good bye': goodbye,
-    'help': get_help,
-    'congratulate': find_birthdays_in_x_days,
-    'add contact': add_contact,
-    'add address': None,
-    'add phone': None,
-    'add email': None,
-    'add birthday': None,
-    'change address': None,
-    'change phone': None,
-    'change email': None,
-    'change birthday': None,
-    'change name': None,
-    'find contact': contacts.find,
-    'delete contact': contacts.delete,
-    'show contacts': None,
-    'add note': None,
-    'find note': None,
-    'change note': None,
-    'delete note': None,
-    'add tag': None,
-    'sort notes': None,
-    'sort dir': None,
-    'make file': None
+    'help': get_help
 }
